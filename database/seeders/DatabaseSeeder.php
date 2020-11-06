ea2235360d7cceeca8964e703d0e1de175f47da4
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-11-06 09:32:26 +0700

    Call all Seeder in Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(EpisodesSeeder::class);
+        $this->call(GenresSeeder::class);
+        $this->call(MoviesSeeder::class);
     }
 }
