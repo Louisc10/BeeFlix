519a99ec3a17b44665e3d073a729cc441da227cd
Louisc10 <42336657+Louisc10@users.noreply.github.com>
2020-11-07 13:36:16 +0700

    Refactoring View

--- resources/views/Page/KidsPage.blade.php
+++ resources/views/Page/KidsPage.blade.php
@@ -8,10 +8,6 @@
     Kids
 @endsection
 
-@section('nav')
-    @include('template.navbar')
-@endsection
-
 @section('content')
 <div style="background-color: darkgray; width: auto; padding: 10px; margin: 5px 0; display: flex; justify-content: center; align-items: center;">
     <div style="background-color: white; width: 100%; padding: 15px">
--- resources/views/Page/OneMoviePage.blade.php
+++ resources/views/Page/OneMoviePage.blade.php
@@ -1,5 +1,7 @@
 @php
     $item;
+    $category;
+    $collection;
 @endphp
 
 @extends('template.template')
@@ -8,18 +10,55 @@
     Home Page
 @endsection
 
-@section('nav')
-    @include('template.navbar')
-@endsection
-
 @section('content')
-<div style="width: 100%; height: 250px;">
-    <img src="{{ URL::to('/') }}/assets/{{ $item['photo'] }}" alt="{{ $item['title'] }}" style="width: 100%; height: 100%;">
-</div>
-<div style="width: 100%; height: 50px; display: flex; justify-content: center; align-items: center; text-align: center">
-    {{ $item['title'] }}
-</div>
-<div style="width: 100%; height: 50px; display: flex; justify-content: center; align-items: center; text-align: center">
-    {{ $item['description'] }}
+<div style="background-color: darkgray; width: auto; padding: 10px; margin: 5px 0; display: flex; justify-content: center; align-items: center;">
+    <div style="background-color: white; width: 100%; padding: 15px; display: flex; flex-wrap: wrap; justify-content: center;align-items: center; flex-direction: row">
+        <div style="width: 100%; max-width: 300px; height: 250px;">
+            <img src="{{ URL::to('/') }}/assets/{{ $item['photo'] }}" alt="{{ $item['title'] }}" style="width: 100%; height: 100%;">
+        </div>
+
+        <div style="width: 400px; margin: 10px 10px; display: flex;flex-direction: column; justify-content: center; align-items: center;">
+            <div style="margin: 10px 0; display: flex;flex-direction: column; justify-content: center; align-items: center; text-align: center; font-weight: bold; font-size: 18px">
+                {{ $item['title'] }}
+                <div style="width: 150px; display: flex; justify-content: space-between; align-items: center; text-align: center; font-weight: bold; font-size: 18px">
+                    @for ($i = 0; $i < $item['rating']; $i++)
+                    <span class="fa fa-star" style="color:yellow;"></span>
+                    @endfor
+                    @for ($i = $item['rating']; $i < 5; $i++)
+                    <span class="fa fa-star"></span>
+                    @endfor
+                </div>
+            </div>
+            <div style="width: 50%; display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center">
+                <div style="color: grey;">
+                    Description
+                </div>
+                {{ $item['description'] }}
+            </div>
+            <div>
+                Kategori: <a href="{{ URL::to('/'.$category) }}"  style="color: #5adbbd; text-decoration: none">{{ $item['name'] }}</a>
+            </div>
+        </div>
+        <div style="width: 400px; height: 250px; font-size: 12px">
+            Episode
+            <table class="table">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">Episode</th>
+                    <th scope="col">Judul</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($collection as $item)
+                        <tr>
+                        <td>Episode {{$item['episode']}}</td>
+                        <td>{{$item['title']}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{ $collection->links() }}
+        </div>
+    </div>
 </div>
 @endsection
